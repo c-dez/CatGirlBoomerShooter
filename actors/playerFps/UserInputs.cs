3ba9ffda634f97d3a62a803988308ddf95ce7720
c-dez <hdezcruzcarlos@gmail.com>
2025-05-06 22:40:35 -0600

    added shift key buffer

--- actors/playerFps/UserInputs.cs
+++ actors/playerFps/UserInputs.cs
@@ -5,21 +5,26 @@ namespace Actors.Players
     {
         [Export] private Node3D camera;
         private Timer jumpTimer;
+        private Timer shiftTimer;
         public Vector3 moveDirection = Vector3.Zero;
         public bool jumpPressed = false;
+        public bool ShiftPressed = false;
 
 
         public override void _Ready()
         {
             jumpTimer = GetNode<Timer>("JumpTimer");
+            shiftTimer = GetNode<Timer>("ShiftTimer");
         }
         public override void _Process(double delta)
         {
             GetMoveDirection();
             JumpBuffer();
+            ShiftBuffer();
+            GD.Print(ShiftPressed);
         }
 
-        
+
         private void GetMoveDirection()
         {
             Vector2 rawInput = Input.GetVector("left", "right", "forward", "backwards");
@@ -36,9 +41,19 @@ namespace Actors.Players
             {
                 jumpTimer.Start();
             }
-            jumpPressed = jumpTimer.TimeLeft > 0 ;
+            jumpPressed = jumpTimer.TimeLeft > 0f;
         }
-        
+
+        private void ShiftBuffer()
+        {
+            if (Input.IsActionJustPressed("shift"))
+            {
+                shiftTimer.Start();
+            }
+            ShiftPressed = shiftTimer.TimeLeft > 0f;
+        }
+
     }
-    
+
 }
+    
