a63f9337fb351f2f35f402969fddee7a609b5e99
c-dez <hdezcruzcarlos@gmail.com>
2025-05-13 17:56:22 -0600

    added label para debugear state machine

--- actors/enemies/baseEnemy/Logic.cs
+++ actors/enemies/baseEnemy/Logic.cs
@@ -7,6 +7,10 @@ namespace Actors.Enemies
         VisionCone visionCone;
         Node3D skin;
         Navigation nav;
+        int state = 0;
+
+        // debug
+        Label stateMachineLabel;
 
         enum BehaviorStateMachine
         {
@@ -24,13 +28,30 @@ namespace Actors.Enemies
             skin = GetNode<Node3D>("../Skin");
             nav = GetNode<Navigation>("../Navigation");
 
+            //debug
+            stateMachineLabel = GetNode<Label>("StateMachineLabel");
+
 
         }
 
         public override void _PhysicsProcess(double delta)
         {
-            GD.Print(visionCone.lastPlayerPosition);
+            StateLabel();
+        }
+
+
+        void StateLabel()
+        {
+            stateMachineLabel.Text = state switch
+            {
+                (int)BehaviorStateMachine.idle => "idle",
+                (int)BehaviorStateMachine.patroling => "patroling",
+                (int)BehaviorStateMachine.investigate => "investigate",
+                (int)BehaviorStateMachine.chase => "chase",
+                _ => "no state",
+            };
         }
     }
+
 
 }
